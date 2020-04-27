11f1da6c7c52b2d43a22f4fcfc462c0bdc082daa
Jackson Yin <Yrh7383111@outlook.com>
2020-04-27 19:49:38 -0400

    Complete Section 12

---
+++ Spring_MVC_Controller_View/src/Controller_and_View/HelloWorldController.java
@@ -0,0 +1,48 @@
+package Controller_and_View;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+
+@Controller
+public class HelloWorldController
+{
+
+	// Need a controller method to show the initial HTML form
+	@RequestMapping("/showForm")
+	public String showForm()
+	{
+		return "helloworld-form";
+	}
+		
+	// Need a controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm()
+	{
+		return "helloworld";
+	}
+
+	// New a controller method to read form data and
+	// Add data to the model
+	@RequestMapping("/processFormVersionTwo")	
+	public String letsShoutDude(HttpServletRequest request, Model model)
+	{
+		// Read the request parameter from the HTML form
+		String name = request.getParameter("studentName");
+		
+		// Convert the data to all caps
+		name = name.toUpperCase();
+		
+		// Create the message
+		String result = "LOL " + name;
+		
+		// Add message to the model
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+}
