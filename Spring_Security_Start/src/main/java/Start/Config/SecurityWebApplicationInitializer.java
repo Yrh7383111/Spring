78f1dd62dc3a050594bd293e57081c0e06ef13b5
Jackson Yin <Yrh7383111@outlook.com>
2020-05-13 16:03:54 -0400

    Complete Section 47

---
+++ Spring_Security_Start/src/main/java/Start/Config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package Start.Config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
+{
+}
