fb749ccfb2902ab3bdc80ca81f60bb012faf7643
Jackson Yin <Yrh7383111@outlook.com>
2020-05-21 17:01:45 -0400

    Complete Section 56 57 58

---
+++ Spring_REST_Controller/src/main/java/Spring_REST_Controller/Config/DemoAppConfig.java
@@ -0,0 +1,16 @@
+package Spring_REST_Controller.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("Spring_REST_Controller")
+public class DemoAppConfig implements WebMvcConfigurer
+{
+
+}
---
+++ Spring_REST_Controller/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h3>Spring REST</h3>
+
+<hr>
+
+<a href="test/hello">Hello</a>
+
+</body>
+</html>
