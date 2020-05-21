cc3234b875ada5e33164e95141f39a7bc258e20f
Jackson Yin <Yrh7383111@outlook.com>
2020-05-21 19:37:52 -0400

    Complete Section 59 60

--- Spring_REST_POJO_to_JSON/src/main/java/Spring_REST_POJO_to_JSON/REST/StudentRestController.java
+++ Spring_REST_POJO_to_JSON/src/main/java/Spring_REST_POJO_to_JSON/REST/StudentRestController.java
@@ -2,9 +2,11 @@ package Spring_REST_POJO_to_JSON.REST;
 
 import Spring_REST_POJO_to_JSON.Entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,15 +16,30 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController 
 {
+	// Private
+	List<Student> students;
+
+
+	// Public
+	@PostConstruct
+	public void loadData()
+	{
+		students = new ArrayList<>();
+
+		students.add(new Student("Poornima", "Patel"));
+		students.add(new Student("Mario", "Rossi"));
+		students.add(new Student("Mary", "Smith"));
+	}
+
 	@GetMapping("/students")
 	public List<Student> getStudents() 
 	{
-		List<Student> students = new ArrayList<>();
-		
-		students.add(new Student("Poornima", "Patel"));
-		students.add(new Student("Mario", "Rossi"));
-		students.add(new Student("Mary", "Smith"));		
-			
 		return students;
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId)
+	{
+		return students.get(studentId);
 	}
 }
